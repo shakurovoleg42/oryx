4de8bbf33953a1cec26877cc665ff981e6ea5597
sellkonAiva <o.shakurov@kontur-sc.ru>
2024-05-02 17:17:39 +0500

    added second section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { FirstIndex } from "./components/FirstSection/FirstIndex";
+import { Order } from "./components/SecondSection/Order/Order";
 
 function App() {
   
   return (
     <>
       <FirstIndex/>
+      <Order/>
     </>
   )
 }
---
+++ src/components/SecondSection/Order/Order.jsx
@@ -0,0 +1,17 @@
+import "./order.scss";
+
+export const Order = () => {
+
+    return (
+        <div className="order">
+            <div className="how-order">
+                <h2 className="how-to-get">Как заказать доставку посылки из США?</h2>
+            </div>
+            <div className="get-now">
+                <a href="#">
+                   <button className="get-now-btn">Получи сейчас</button> 
+                </a>
+            </div>
+        </div>
+    )
+}
