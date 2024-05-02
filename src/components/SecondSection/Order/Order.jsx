077f40948eb2f3ce4068dc779e52ebf038e871c7
sellkonAiva <o.shakurov@kontur-sc.ru>
2024-05-02 17:59:52 +0500

    .

--- src/components/SecondSection/Order/Order.jsx
+++ src/components/SecondSection/Order/Order.jsx
@@ -7,6 +7,21 @@ export const Order = () => {
             <div className="how-order">
                 <h2 className="how-to-get">Как заказать доставку посылки из США?</h2>
             </div>
+            <div>
+                <img src="/src/assets/img/icons/registration.png" alt="registration-icon"
+                    style={{
+                        "width": "73px",
+                        "height": "73px"
+                    }}
+                />
+                <img src="/src/assets/img/icons/go_buy.png" alt="go-buy-icon"
+                    style={{
+                        "width": "73px",
+                        "height": "73px"
+                    }}
+                />
+                <img src="" alt="get-icon" />
+            </div>
             <div className="get-now">
                 <a href="#">
                    <button className="get-now-btn">Получи сейчас</button> 
