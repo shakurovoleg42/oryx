2ba8c8b25240a465f816c89506fe3d86cfc09197
shakurovoleg42 <thorsonodin2003@gmail.com>
2024-05-17 18:04:37 +0500

    .

--- src/components/AboutCompany/About.jsx
+++ src/components/AboutCompany/About.jsx
@@ -3,7 +3,19 @@ import "./about.scss";
 export const About = () => {
     return (
         <div className="about_company">
+            <div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
             
+            <div>
+                <button></button>
+            </div>
         </div>
     )
 }
