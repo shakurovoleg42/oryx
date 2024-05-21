5c9a4e02b960b347d0edcf6bf12e1452e98d35cb
shakurovoleg42 <thorsonodin2003@gmail.com>
2024-05-21 12:23:34 +0500

    know more

--- src/components/AboutCompany/About.jsx
+++ src/components/AboutCompany/About.jsx
@@ -4,18 +4,23 @@ export const About = () => {
     return (
         <div className="about_company">
             <div>
-                <div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div>
-                    
-                </div>
+                    <div className="title_section">
+                        <p className="title">
+                            О компании
+                        </p>
+                    </div>
+                    <div className="text_section">
+                        <p className="text">
+                            ORYX – это надежный мейлфорвардер, имеющий собственный автоматизированный склад в безналоговом штате Америки. Покупка и доставка из США любых товаров – наша основная деятельность. Мы сотрудничаем с лучшими перевозчиками долго и плодотворно благодаря этому готовы предложить доступную стоимость доставки.
+                        </p>
+                    </div>
+                    <button className="know_more">
+                        Узнать больше
+                        <img className="arrowtomore" src="/src/assets/img/elements/arrowtomore.png" alt="arrowtomore"/>
+                    </button>
             </div>
-            
-            <div>
-                <button></button>
-            </div>
+                <img className="big_car" src="/src/assets/img/images/big_car.png" alt="big_car" style={{width: "445px", height: "325px"}}/>
         </div>
     )
 }
+// className="title_text_section"
