7fcf8ae20cd2b6ab4046c9adcf3c61ecd3aa678f
shakurovoleg42 <thorsonodin2003@gmail.com>
2024-05-23 12:55:50 +0500

    fixed second section

--- src/components/SecondSection/Order/Order.jsx
+++ src/components/SecondSection/Order/Order.jsx
@@ -6,41 +6,41 @@ export const Order = () => {
         <div className="how-order">
           <h2 className="how-to-get">Как заказать доставку посылки из США?</h2>
         </div>
-        <div className="clue">
-          <div className="clue-item">
-            <img
-              src="/src/assets/img/icons/registration.png"
-              alt="icon"
-              className="clue-item-img"
-            />
-            <div className="about-paragraph">
-              <p className="name-paragraph">Зарегистрируйтесь</p>
-              <p className="info-paragraph">Получите адрес для доставки посылок в США</p>
+          <div className="clue">
+            <div className="clue-item">
+              <img
+                src="/src/assets/img/icons/registration.png"
+                alt="icon"
+                className="clue-item-img"
+              />
+              <div className="about-paragraph">
+                <p className="name-paragraph">Зарегистрируйтесь</p>
+                <p className="info-paragraph">Получите адрес для доставки посылок в США</p>
+              </div>
+            </div>
+            <div className="clue-item" style={{marginLeft: "178px", marginRight: "177px"}}>
+              <img
+                src="/src/assets/img/icons/go_buy.png"
+                alt="icon"
+                className="clue-item-img"
+              />
+              <div className="about-paragraph">
+                <p className="name-paragraph">Покупайте</p>
+                <p className="info-paragraph">Заказывайте онлайн и отправляйте посылки на наш склад</p>
+              </div>
+            </div>
+            <div className="clue-item">
+              <img
+                src="/src/assets/img/icons/get.png"
+                alt="icon"
+                className="clue-item-img"
+              />
+              <div className="about-paragraph">
+                <p className="name-paragraph">Получайте</p>
+                <p className="info-paragraph">Ваши посылки доставят в течение 7-14 дней</p>
+              </div>
             </div>
           </div>
-          <div className="clue-item" style={{marginLeft: "178px", marginRight: "177px"}}>
-            <img
-              src="/src/assets/img/icons/go_buy.png"
-              alt="icon"
-              className="clue-item-img"
-            />
-            <div className="about-paragraph">
-              <p className="name-paragraph">Покупайте</p>
-              <p className="info-paragraph">Заказывайте онлайн и отправляйте посылки на наш склад</p>
-            </div>
-          </div>
-          <div className="clue-item">
-            <img
-              src="/src/assets/img/icons/get.png"
-              alt="icon"
-              className="clue-item-img"
-            />
-            <div className="about-paragraph">
-              <p className="name-paragraph">Получайте</p>
-              <p className="info-paragraph">Ваши посылки доставят в течение 7-14 дней</p>
-            </div>
-          </div>
-        </div>
         <div className="get-now">
           <a href="#!">
             <button className="get-now-btn">Получи сейчас</button>
