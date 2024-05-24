3476649386ed7642671e967ff89463d191b9ee4c
shakurovoleg42 <thorsonodin2003@gmail.com>
2024-05-24 17:52:21 +0500

    added function Scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Main } from './Pages/Main';
 import { About } from './Pages/About/About';
 import { PopularShops } from './Pages/PopularShops/PopularShops'
+import ScrollToTop from './components/functions/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />
