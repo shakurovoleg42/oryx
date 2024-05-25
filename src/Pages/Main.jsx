58995cc1608d3df91939f6af6e37d6d40e9f3216
Oleg Shakurov <121859456+shakurovoleg42@users.noreply.github.com>
2024-05-26 00:09:44 +0500

    Update Main.jsx

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,4 +1,4 @@
-import { FirstIndex } from "../components/FirstSection/FirstIndex";
+import { FirstIndex } from "../components/firstSection/firstIndex";
 import { Order } from "../components/SecondSection/Order/Order";
 import { Popular } from "../components/ThirdSection/Popular";
 import { CalcSection } from "../components/CalcSection/CalcSection";
