586a56c5ca241127333b59c7c92d8666bb4900ee
shakurovoleg42 <thorsonodin2003@gmail.com>
2024-05-30 15:10:45 +0500

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '',  // Использовать относительные пути
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-  server: {
-    mimeTypes: {
-      'application/javascript': ['js', 'jsx']
-    }
   },
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
 });
