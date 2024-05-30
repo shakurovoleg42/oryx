7761ce0e1235de7c3f2a3e603de7d3807c777753
shakurovoleg42 <thorsonodin2003@gmail.com>
2024-05-30 12:11:04 +0500

    /

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-// Создаем корень приложения
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Рендерим приложение в корень
 root.render(
     <React.StrictMode>
         <App />
